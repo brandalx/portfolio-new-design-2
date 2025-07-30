9e25dcf8868c8eea20b6c26def68bf8d2793af95
brndalx <113251192+brandalx@users.noreply.github.com>
2025-07-30 00:09:51 -0500

    fix: hero image

--- src/components/maskedDivMain.tsx
+++ src/components/maskedDivMain.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-
 import MaskedDiv from "./ui/masked-div";
 
 const MaksedDivDemo = () => {
   return (
-    <div className="items-between m-auto marquee-hero  mb-10 flex max-w-5xl flex-wrap justify-between gap-5">
+    <div className="items-between m-auto marquee-hero mb-10 flex max-w-5xl flex-wrap justify-between gap-5">
       <MaskedDiv maskType={"type-1"} size={0.45} className="my-4">
         <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
+          className="pointer-events-none" // Disables all interactions
           autoPlay
           loop
           muted
+          playsInline // Prevents iOS full-screen
+          controls={false} // Hides controls
         >
           <source
             src="https://videos.pexels.com/video-files/7710243/7710243-uhd_2560_1440_30fps.mp4"
@@ -18,12 +19,14 @@ const MaksedDivDemo = () => {
           />
         </video>
       </MaskedDiv>
-      <MaskedDiv maskType="type-1" size={0.45} className="rotate-180 ">
+      <MaskedDiv maskType="type-1" size={0.45} className="rotate-180">
         <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
+          className="pointer-events-none"
           autoPlay
           loop
           muted
+          playsInline
+          controls={false}
         >
           <source
             src="https://videos.pexels.com/video-files/18069803/18069803-uhd_1440_2560_24fps.mp4"
@@ -33,10 +36,12 @@ const MaksedDivDemo = () => {
       </MaskedDiv>
       <MaskedDiv maskType="type-3" className="my-4">
         <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
+          className="pointer-events-none"
           autoPlay
           loop
           muted
+          playsInline
+          controls={false}
         >
           <source
             src="https://videos.pexels.com/video-files/18069166/18069166-uhd_2560_1440_24fps.mp4"
@@ -46,10 +51,12 @@ const MaksedDivDemo = () => {
       </MaskedDiv>
       <MaskedDiv maskType="type-4" className="my-4">
         <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
+          className="pointer-events-none"
           autoPlay
           loop
           muted
+          playsInline
+          controls={false}
         >
           <source
             src="https://videos.pexels.com/video-files/18069701/18069701-uhd_2560_1440_24fps.mp4"
@@ -59,10 +66,12 @@ const MaksedDivDemo = () => {
       </MaskedDiv>
       <MaskedDiv maskType="type-2" className="my-4">
         <video
-          className="cursor-pointer transition-all duration-300 hover:scale-105"
+          className="pointer-events-none"
           autoPlay
           loop
           muted
+          playsInline
+          controls={false}
         >
           <source
             src="https://videos.pexels.com/video-files/18069232/18069232-uhd_2560_1440_24fps.mp4"
